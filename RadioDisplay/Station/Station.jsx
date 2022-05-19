d6d69bacbb2e3dab564013a24cd3668a21f5b2e1
WebFilin <filimigr@gmail.com>
2022-05-19 22:38:23 +0500

    Мелкие правки верстки

--- RadioDisplay/Station/Station.jsx
+++ RadioDisplay/Station/Station.jsx
@@ -11,20 +11,27 @@ function Station({ contryName, min, max, dot }) {
    // Положение скрола выбора радиостанции
    const valueTuning = useSelector(state => state.tuning.tuning)
 
+   const playPauseSwitch = useSelector(state => state.urlAudioSourse.playPauseSwitch)
+
    // Включаем подсветку в зависимости от положения ползунка
    React.useEffect(() => {
 
       let station = stationRef.current
 
-      if (valueTuning >= min && valueTuning <= max) {
-         station.classList.add("radio__display-station-neon")
+      function scrollBacklight() {
+         if (valueTuning >= min && valueTuning <= max) {
+            station.classList.add("radio__display-station-neon")
+         }
+
+         else {
+            station.classList.remove("radio__display-station-neon")
+         }
       }
 
-      else {
-         station.classList.remove("radio__display-station-neon")
-      }
+      // Отключаем подсветку станции при выключенной музыке
+      playPauseSwitch ? scrollBacklight() : station.classList.remove("radio__display-station-neon")
 
-   }, [valueTuning, min, max, dot])
+   }, [valueTuning, min, max, dot, playPauseSwitch])
 
    return (
 
@@ -34,7 +41,7 @@ function Station({ contryName, min, max, dot }) {
          {dot === "up" && <div className='radio__station-point_range'></div>}
 
          <div ref={stationRef} className="radio__station-body">
-            {contryName && contryName}
+            {contryName}
          </div>
 
          {dot === "down" && <div className='radio__station-point_range'></div>}
